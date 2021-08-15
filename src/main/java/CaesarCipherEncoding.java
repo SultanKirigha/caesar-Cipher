cc3059cfba49ae4eca543df2b4f4b6ad84f090f8
“SultanMwangoyaKirigha” <sultan.kirigha@student.moringaschool.com>
2021-08-15 10:14:02 +0300

    wrote a encoding programme

--- src/main/java/CaesarCipherEncoding.java
+++ src/main/java/CaesarCipherEncoding.java
@@ -1,2 +1,41 @@
-public class CaesarCipherEncoding {
+import java.util.Scanner;
+
+public class CaesarCipherEncoding{
+
+    static Scanner input = new Scanner(System.in);
+
+
+    public static void main(String[] args){
+
+        System.out.println("Enter A text: ");
+        String plain = input.nextLine();
+
+        System.out.print("Enter shift key: ");
+        int shift = input.nextInt();
+        String cipher = encode(plain,shift);
+
+
+        System.out.println("Cipher text is:  " + cipher);
+
+    }
+    public static String encode(String plain,int shift){
+
+
+        String p = plain.toLowerCase();
+        char chr [] = p.toCharArray();
+        int key = shift;
+
+        int number;
+        String str="";
+        char cipher;
+
+        for (int i = 0; i < chr.length; i++){
+
+            number = chr[i] -97;
+            number = (number + key + 26) % 26;
+            cipher = (char) (number + 97);
+            str = str + cipher;
+        }
+        return str;
+    }
 }
